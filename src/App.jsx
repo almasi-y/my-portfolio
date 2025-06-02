0fa3bf0248bc8b05bf932e3a91f6913805591373
almasi-y <133103054+almasi-y@users.noreply.github.com>
2025-06-02 14:46:10 +0300

    :wq

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import {About} from './pages/About';
 import {Skills }from './pages/Skills';
